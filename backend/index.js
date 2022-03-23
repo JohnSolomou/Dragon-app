394e581ecc122de2d10baaf2729b1b6ac4fd4502
John Solomou <solomoufam@yahoo.com>
2022-03-22 20:45:10 -0400

    added generation

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,34 @@
-const Dragon = require("./dragon");
+// const Dragon = require("./dragon");
 
-const fooey = new Dragon({
-  birthdate: new Date(),
-  nickname: "fooey",
-});
-const baloo = new Dragon({
-  birthdate: new Date(),
-  nickname: "baloo",
-  traits: [{ traitType: "backgroundColor", traitValue: "green" }],
-});
+// const fooey = new Dragon({
+//   birthdate: new Date(),
+//   nickname: "fooey",
+// });
+// const baloo = new Dragon({
+//   birthdate: new Date(),
+//   nickname: "baloo",
+//   traits: [{ traitType: "backgroundColor", traitValue: "green" }],
+// });
 
-const mimar = new Dragon();
+// const mimar = new Dragon();
+// setTimeout(() => {
+//   const gooby = new Dragon();
+//   console.log("gooby", gooby);
+// }, 3000);
+
+// console.log("fooey", fooey);
+// console.log("baloo", baloo);
+// console.log("mimar", mimar);
+
+const Generation = require("./generation");
+
+const generation = new Generation();
+console.log("generation", generation);
+
+const gooby = generation.newDragon();
+console.log("gooby", gooby);
+
 setTimeout(() => {
-  const gooby = new Dragon();
-  console.log("gooby", gooby);
-}, 3000);
-
-console.log("fooey", fooey);
-console.log("baloo", baloo);
-console.log("mimar", mimar);
+  const mimar = generation.newDragon();
+  console.log("mimar", mimar);
+}, 15000);
